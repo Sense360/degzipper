04939fd02d0754b8fb65fbb977c5629c3deb3f92
Eric Fornaciari <ericf@sense360.com>
2019-06-14 14:55:45 -0700

    BEAR-4133: Update Degzipper middleware to be Rails 5 Compatible

--- lib/degzipper/rails.rb
+++ lib/degzipper/rails.rb
@@ -1,7 +1,7 @@
 module Degzipper
   class Rails < Rails::Railtie
     initializer "degzipper.configure_rails_initialization" do |app|
-      app.middleware.insert_before ActionDispatch::ParamsParser, "Degzipper::Middleware"
+      app.middleware.insert_before Rack::Head, Degzipper::Middleware
     end
   end
 end
